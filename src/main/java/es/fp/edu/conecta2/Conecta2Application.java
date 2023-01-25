fc42b024496f628558b85daeb27b30605db8f1be
Juanjo <juanjo@fp.edu.es>
2023-01-25 11:37:55 +0100

    Inicial

---
+++ src/main/java/es/fp/edu/conecta2/Conecta2Application.java
@@ -0,0 +1,13 @@
+package es.fp.edu.conecta2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Conecta2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Conecta2Application.class, args);
+    }
+
+}
---
+++ src/test/java/es/fp/edu/conecta2/Conecta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fp.edu.conecta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Conecta2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
