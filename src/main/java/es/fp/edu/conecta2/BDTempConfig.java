91da37c7ff63d79278c24db1ec26eefb4aabc4c4
Juanjo <juanjo@fp.edu.es>
2023-01-27 19:08:08 +0100

    conectando a H2

--- src/main/java/es/fp/edu/conecta2/BDTempConfig.java
+++ src/main/java/es/fp/edu/conecta2/BDTempConfig.java
@@ -1,9 +1,9 @@
 package es.fp.edu.conecta2;
 
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -16,47 +16,49 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories (
-        entityManagerFactoryRef = "userEntityManagerFactory",
-        transactionManagerRef = "userTransactionManger",
-        basePackages = {"es.fp.edu.conecta2.repo.user"})
-public class BDConfig {
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "adminEntityManagerFactory",
+        transactionManagerRef = "adminTransactionManger",
+        basePackages = {"es.fp.edu.conecta2.repo.admin"})
+
+public class BDTempConfig {
     @Autowired
     private Environment env;
 
-    @Bean(name="userDataSource")
+    @Bean(name="adminDataSource")
     public DataSource userDatasource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(env.getProperty("persistente.datasource.url"));
-        dataSource.setUsername(env.getProperty("persistente.datasource.username"));
-        dataSource.setPassword(env.getProperty("persistente.datasource.password"));
-        dataSource.setDriverClassName(env.getProperty("persistente.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("temporal.datasource.url"));
+        dataSource.setUsername(env.getProperty("temporal.datasource.username"));
+        dataSource.setPassword(env.getProperty("temporal.datasource.password"));
+        dataSource.setDriverClassName(env.getProperty("temporal.datasource.driver-class-name"));
 
         return dataSource;
 
     }
-    @Bean(name = "userEntityManagerFactory")
+    @Bean(name = "adminEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         // TODO: 27/01/2023
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(userDatasource());
-        em.setPackagesToScan("es.fp.edu.conecta2.modelo.user");
+        em.setPackagesToScan("es.fp.edu.conecta2.modelo.admin");
 
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
 
         Map<String,Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", env.getProperty("persistente.jpa.hibernate.ddl-auto"));
-        properties.put("hibernate.show-sql", env.getProperty("persistente.jpa.show-sql"));
-        properties.put("hibernate.dialect", env.getProperty("persistente.jpa.properties.hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("temporal.jpa.hibernate.ddl-auto"));
+        properties.put("hibernate.show-sql", env.getProperty("temporal.jpa.show-sql"));
+        properties.put("hibernate.dialect", env.getProperty("temporal.jpa.properties.hibernate.dialect"));
 
         em.setJpaPropertyMap(properties);
         return em;
     }
 
-    @Bean(name = "userTransactionManager")
+    @Bean(name = "adminTransactionManager")
     public PlatformTransactionManager transactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
@@ -68,3 +70,4 @@ public class BDConfig {
 
 
 }
+
--- src/main/java/es/fp/edu/conecta2/repo/admin/IAdminRepo.java
+++ src/main/java/es/fp/edu/conecta2/repo/admin/IAdminRepo.java
@@ -3,6 +3,6 @@ package es.fp.edu.conecta2.repo.admin;
 import es.fp.edu.conecta2.modelo.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IAdminRepo extends JpaRepository <Admin, Integer> {
+public interface IAdminRepo extends JpaRepository<Admin, Integer> {
 
 }
