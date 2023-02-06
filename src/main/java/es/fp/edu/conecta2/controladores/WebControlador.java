15d132a8f3c4673fdcfbb70991d25604e800ce6b
Juanjo <juanjo@fp.edu.es>
2023-02-06 17:02:35 +0100

    Create - controlardor Guardar

--- src/main/java/es/fp/edu/conecta2/controladores/WebControlador.java
+++ src/main/java/es/fp/edu/conecta2/controladores/WebControlador.java
@@ -7,9 +7,7 @@ import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -29,6 +27,11 @@ public class WebControlador {
         User usuario = new User();
         modelo.addAttribute("user",usuario);
         return "nuevo_usuario";
+    }
+    @PostMapping("/guardar")
+    public String guardarUser (@ModelAttribute("user") User usuario) {
+        servicioUser.registrar(usuario);
+        return "redirect:/";
     }
 
     @GetMapping ("/borrar")
